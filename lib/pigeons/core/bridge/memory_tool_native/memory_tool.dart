07e1d35d4cb4efdc06825ace869a9e8fab64cccf
挽风 <a3113143666@gmail.com>
2026-04-13 01:51:18 +0800

    feat(memory_tool): add ProcessInfo class and getProcessInfo method

--- lib/pigeons/core/bridge/memory_tool_native/memory_tool.dart
+++ lib/pigeons/core/bridge/memory_tool_native/memory_tool.dart
@@ -1,7 +1,24 @@
 import 'package:pigeon/pigeon.dart';
 
+class ProcessInfo {
+  final int pid;
+  final String name;
+  final String packageName;
+  final Uint8List? icon;
+
+  const ProcessInfo({
+    required this.pid,
+    required this.packageName,
+    required this.name,
+    this.icon,
+  });
+}
+
 @HostApi()
 abstract class MemoryToolNative {
   @async
   int getPid({required String packageName});
+
+  @async
+  List<ProcessInfo> getProcessInfo(int offset, int limit);
 }
